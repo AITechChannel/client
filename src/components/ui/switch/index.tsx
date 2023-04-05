5b042641709f5e0099798e4682a3b0f322a81628
MD-TuanAnhDoan <anh.DT@mor.com.vn>
2023-04-05 10:09:26 +0700

    fix import icons

--- src/components/ui/switch/index.tsx
+++ src/components/ui/switch/index.tsx
@@ -1,24 +1,23 @@
-import React from "react";
-import { IconLightDark } from "../icons";
-import PropsType from "./interface";
-import styles from "./style.module.scss";
+import React from 'react';
+import PropsType from './interface';
+import styles from './style.module.scss';
 
 function Switch(props: PropsType) {
   const { status, onChange, icon } = props;
 
   return (
     <div
-      className={`${styles["switch-wrapper"]} ${
-        status === 1 ? styles.active : ""
+      className={`${styles['switch-wrapper']} ${
+        status === 1 ? styles.active : ''
       }`}
       onClick={onChange}
     >
       {icon ? (
-        <div className={`${styles.icon} ${status === 1 ? styles.active : ""}`}>
+        <div className={`${styles.icon} ${status === 1 ? styles.active : ''}`}>
           {icon}
         </div>
       ) : (
-        <div className={`${styles.dot} ${status === 1 ? styles.active : ""}`} />
+        <div className={`${styles.dot} ${status === 1 ? styles.active : ''}`} />
       )}
     </div>
   );
